4fe61a2fcd17ee11745996530468b87b61ef9888
andrewschreiber <andrew.schreiber1@gmail.com>
2020-02-03 01:33:19 -0800

    run: TEST non deterministic speed

--- training.py
+++ training.py
@@ -77,8 +77,8 @@ if __name__ == "__main__":
         random.seed(seed)
         np.random.seed(seed)
         os.environ["PYTHONHASHSEED"] = str(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = False
+        # torch.backends.cudnn.benchmark = False
     shuffle = not deterministic
 
     print("Deterministic:", deterministic)
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     # print("modules: algebra", mdsmgr.get_modules_for_category("algebra"))
 
     exp_name = "math_112m_bs128"
-    unique_id = "1-25-20_1"
+    unique_id = "2-3-20_TEST"
 
     # ds_train = mdsmgr.build_dataset_from_module(
     #     "algebra", "linear_1d", "train-easy", max_elements=max_elements
@@ -162,7 +162,7 @@ if __name__ == "__main__":
         "./mathematics_dataset-v1.0",
         max_elements=max_elements,
         deterministic=deterministic,
-        start_epoch=epoch,
+        start_epoch=2,
     )
     print("Train dataset size", len(ds_train))
 
