232aa0580771b7f597ac1680d4939951a1351af5
Taylor K <taylorkmc@gmail.com>
2020-02-24 20:40:15 -0800

    Refactoring LSTM training code

--- training_lstm.py
+++ training_lstm.py
@@ -2,10 +2,13 @@
   code by Tae Hwan Jung(Jeff Jung) @graykode
 '''
 import numpy as np
+from tensorboard_utils import Tensorboard
+import multiprocessing
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+import model_process
 from math_dataset import (
     question_answer_to_position_batch_collate_fn,
     MathDatasetManager,
@@ -14,38 +17,37 @@ from math_dataset import (
 
 dtype = torch.FloatTensor
 
-# char_arr = [c for c in 'abcdefghijklmnopqrstuvwxyz']
-# word_dict = {n: i for i, n in enumerate(char_arr)}
-# number_dict = {i: w for i, w in enumerate(char_arr)}
-# n_class = len(word_dict) # number of class(=number of vocab)
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+#cudnn.benchmark = True
 
-# seq_data = ['cry', 'boy', 'jay', 'mew', 'cat', 'and', 'his', 'lie', 'big', 'sit']
-
+# Uni-LSTM(Attention) Parameters
+embedding_dim = 2
+n_hidden = 5 # number of hidden units in one cell
+num_classes = 2  # 0 or 1
 max_elements = 10
+vocab_size = 10
+n_step = 3
+n_hidden = 128
+max_batches = 2
+num_workers = 0
+
+exp_name = "math_test"
+unique_id = "1-13-2020"
+
+tb = Tensorboard(exp_name, unique_name=unique_id)
 
 mdsmgr = MathDatasetManager("./mathematics_dataset-v1.0")
 ds_train = mdsmgr.build_dataset_from_module("algebra", "linear_1d", "train-easy", max_elements=max_elements)
-ds_train =  ds_train = FullDatasetManager(
-        "./mathematics_dataset-v1.0", max_elements=max_elements
-    )
+# ds_train = FullDatasetManager(
+#         "./mathematics_dataset-v1.0", max_elements=max_elements
+#     )
 
-dataloader = torch.utils.data.DataLoader(ds_train, batch_size=1024,
-                        shuffle=True, num_workers=0)    
+train_loader = torch.utils.data.DataLoader(ds_train, batch_size=1024,
+                        shuffle=True, num_workers=num_workers)    
 
-# TextLSTM Parameters
-n_step = 3
-n_hidden = 128
-
-def make_batch(seq_data):
-    input_batch, target_batch = [], []
-
-    for seq in seq_data:
-        input = [word_dict[n] for n in seq[:-1]] # 'm', 'a' , 'k' is input
-        target = word_dict[seq[-1]] # 'e' is target
-        input_batch.append(np.eye(n_class)[input])
-        target_batch.append(target)
-
-    return Variable(torch.Tensor(input_batch)), Variable(torch.LongTensor(target_batch))
+#Define Model Architecture 
 
 class TextLSTM(nn.Module):
     def __init__(self):
@@ -66,28 +68,38 @@ class TextLSTM(nn.Module):
         model = torch.mm(outputs, self.W) + self.b  # model : [batch_size, n_class]
         return model
 
-input_batch, target_batch = make_batch(seq_data)
-
 model = TextLSTM()
 
+# Specify optimizations algs 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-output = model(input_batch)
+model_process.train(
+        exp_name=exp_name,
+        unique_id=unique_id,
+        model=model,
+        training_data=train_loader,
+        optimizer=optimizer,
+        device=device,
+        epochs=1,
+        tb=tb,
+        max_batches=max_batches,
+        validation_data=None,
+    )
 
-# Training
-for epoch in range(1000):
-    optimizer.zero_grad()
+# Training (Old School)
+# for epoch in range(1000):
+#     optimizer.zero_grad()
 
-    output = model(input_batch)
-    loss = criterion(output, target_batch)
-    if (epoch + 1) % 100 == 0:
-        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+#     output = model(input_batch)
+#     loss = criterion(output, target_batch)
+#     if (epoch + 1) % 100 == 0:
+#         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
 
-    loss.backward()
-    optimizer.step()
+#     loss.backward()
+#     optimizer.step()
 
-inputs = [sen[:3] for sen in seq_data]
+# inputs = [sen[:3] for sen in seq_data]
 
-predict = model(input_batch).data.max(1, keepdim=True)[1]
-print(inputs, '->', [number_dict[n.item()] for n in predict.squeeze()])
+# predict = model(input_batch).data.max(1, keepdim=True)[1]
+# print(inputs, '->', [number_dict[n.item()] for n in predict.squeeze()])
