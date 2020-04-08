a5a01d2f65b1a1a4aad14c4f5f9f8b2b92baad46
andrewschreiber <andrew.schreiber1@gmail.com>
2020-04-08 01:05:50 -0400

    feat: create folders

--- gce/prestartup.sh
+++ gce/prestartup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-echo "Running prestartup script..."
-echo "....................................."
-wget https://github.com/andrewschreiber/hs-math-nlp/archive/master.zip
+echo "Running prestartup script..."       
 
-unzip master.zip
+if [[ ! -d "hs-math-nlp-master"]]
+then
+  echo "Setting up instance"
+  wget https://github.com/andrewschreiber/hs-math-nlp/archive/master.zip
 
-cd hs-math-nlp-master && ls
+  unzip master.zip
 
-gsutil cp gs://math-checkpoints-data/mathematics_dataset-v1.0.tar.gz dataset.zip
+  cd hs-math-nlp-master && ls
 
-tar xvzf dataset.zip
+  gsutil cp gs://math-checkpoints-data/mathematics_dataset-v1.0.tar.gz dataset.zip
 
-chmod +x gce/startup.sh
+  tar xvzf dataset.zip
 
+  chmod +x gce/startup.sh
+  mkdir runs
+  mkdir checkpoints
+
+fi
 su - andrew_schreiber1 -c '/hs-math-nlp-master/gce/startup.sh'
 
 # andrew_schreiber1
