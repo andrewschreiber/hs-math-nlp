560b40d471dc3ed0021e82ae915c298642905e47
andrewschreiber <andrew.schreiber1@gmail.com>
2020-04-09 17:43:01 -0400

    try: create file

--- gce/shutdown_test.sh
+++ gce/shutdown_test.sh
@@ -2,6 +2,11 @@
 
 echo "++++++++++++++ Shutdown test +++++++++++++++++"
 
+ofile=/var/tmp/shutdown.txt 
+echo "id = $(id)" > $ofile 
+echo "script_file path = $(realpath $0)" >> $ofile 
+echo "script_file rights, user, group = $(stat -c "%A %U %G" $0)" >> $ofile
+
 sleep 30
 
 echo "post sleep"
