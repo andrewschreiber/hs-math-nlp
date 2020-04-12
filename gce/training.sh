f383844c2f2c47319bbea2c04667f0f14e5d592b
andrewschreiber <andrew.schreiber1@gmail.com>
2020-04-12 19:28:15 -0400

    feat: direct at to serial output

--- gce/training.sh
+++ gce/training.sh
@@ -8,12 +8,13 @@ pip install --user tensorboard tensorboardX
 
 python --version
 echo "~~~~~~~ Start training ~~~~~~~"
+tty
 
 
 # Use 'at' to allow the python execution to run async
 # Needed because preemption will insta-kill the startup script
 # nohup still runs under startup script
 
-echo "python training.py" | at now
+echo "python training.py" >  /dev/pts/0 | at now
 
 echo "~~~~~~~ Completed startup script ~~~~~~~"
