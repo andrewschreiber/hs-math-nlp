5956d9b579abeae6fab5f85dc4ae4793fe60a8ec
andrewschreiber <andrew.schreiber1@gmail.com>
2020-04-13 20:01:01 -0400

    feat: try with full dataset

--- gce/startup.sh
+++ gce/startup.sh
@@ -25,10 +25,10 @@ echo "Appending gce/shutdown.sh to $IMAGE_SHUTDOWN_SCRIPT..."
 sudo cat gce/shutdown.sh >> $IMAGE_SHUTDOWN_SCRIPT
 
 # Full dataset
-# gsutil cp gs://math-checkpoints-data/mathematics_dataset-v1.0.tar.gz dataset.zip
+gsutil cp gs://math-checkpoints-data/mathematics_dataset-v1.0.tar.gz dataset.zip
  
 # 10kb dataset for faster testing
-gsutil cp gs://math-checkpoints-data/mini_mathematics_dataset-v1.0.tar.gz dataset.zip
+# gsutil cp gs://math-checkpoints-data/mini_mathematics_dataset-v1.0.tar.gz dataset.zip
 
 tar xvzf dataset.zip
 
