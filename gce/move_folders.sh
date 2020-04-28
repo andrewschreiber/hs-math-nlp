d27909978e91364ff979799544912be0b637eed1
andrewschreiber <andrew.schreiber1@gmail.com>
2020-04-27 20:15:39 -0400

    feat: script for instance hot code updating

---
+++ gce/move_folders.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mv hs-math-nlp-master m2
+
+rm master.zip
+
+wget https://github.com/andrewschreiber/hs-math-nlp/archive/master.zip
+
+unzip master.zip
+
+sudo chmod -R 777 hs-math-nlp-master
+
+mv m2/runs hs-math-nlp-master
+
+mv m2/checkpoints hs-math-nlp-master
+
+mv m2/mathematics_dataset-v1.0 hs-math-nlp-master
+
+cd hs-math-nlp-master
+
+ls
+ 
