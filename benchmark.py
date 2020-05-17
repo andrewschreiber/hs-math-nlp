01584307d12489ea976844efd6b74fa7436b8a62
andrewschreiber <andrew.schreiber1@gmail.com>
2020-05-16 23:47:26 -0400

    fix: remove training code

--- benchmark.py
+++ benchmark.py
@@ -61,7 +61,6 @@ for module, dataset in benchmark.get_datasets("interpolate").items():
             )
             gold_as = batch_as[:, 1:]
 
-            # forward
             pred_as = model(batch_qs, batch_qs_pos, batch_as, batch_as_pos)
             loss, n_correct = compute_performance(pred_as, gold_as, smoothing=False)
 
