2cef3187044da75a3cf8ece9a661c769754b8609
andrewschreiber <andrew.schreiber1@gmail.com>
2020-06-08 22:52:29 -0400

    run: 6-9-20_transformer

--- training.py
+++ training.py
@@ -98,13 +98,13 @@ def main():
     print("Deterministic:", deterministic)
 
     exp_name = "math_112m_bs128"
-    unique_id = "6-8-20_transformer"
+    unique_id = "6-9-20_transformer"
 
     model = utils.build_transformer()
 
     lr = 6e-4
     warmup_lr = 6e-6  # TODO: Refactor into custom optimizer class
-    warmup_interval = None
+    warmup_interval = 20000
     beta_coeff_low = 0.9
     beta_coeff_high = 0.995
     eps = 1e-9
