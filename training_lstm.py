70487f1bce5fdcf394fe9cda3a864d686ae2323e
andrewschreiber <andrew.schreiber1@gmail.com>
2020-06-14 22:50:56 -0400

    feat: catch errors, refactor

--- training_lstm.py
+++ training_lstm.py
@@ -4,6 +4,7 @@
 import numpy as np
 from tensorboard_utils import Tensorboard
 import multiprocessing
+import traceback
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -17,44 +18,14 @@ from math_dataset import (
 
 from math_dataset import VOCAB_SZ, MAX_QUESTION_SZ, MAX_ANSWER_SZ
 
-dtype = torch.FloatTensor
 
-# CUDA for PyTorch
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
-print(device)
-# cudnn.benchmark = True
-
-# Uni-LSTM(Attention) Parameters
-max_elements = 100
-num_hidden = 2048
-max_batches = 1
-num_workers = 2
-
-exp_name = "math_test"
-unique_id = "02-24-2020"
-
-tb = Tensorboard(exp_name, unique_name=unique_id)
-
-ds_train = FullDatasetManager(
-    "./mathematics_dataset-v1.0", max_elements=10, deterministic=True,
-)
-train_loader = torch.utils.data.DataLoader(
-    ds_train,
-    batch_size=16,
-    shuffle=True,
-    num_workers=num_workers,
-    collate_fn=lstm_batch_collate_fn,
-)
-
-
-# Define Model Architecture
 class TextLSTM(nn.Module):
     def __init__(self):
         super(TextLSTM, self).__init__()
-        self.lstm = nn.LSTM(VOCAB_SZ, num_hidden, 1)
+        self.num_hidden = 2048
 
-        self.tgt_word_prj = nn.Linear(2048, VOCAB_SZ, bias=False)
+        self.lstm = nn.LSTM(VOCAB_SZ, self.num_hidden, 1)
+        self.tgt_word_prj = nn.Linear(self.num_hidden, VOCAB_SZ, bias=False)
         self.q_to_a = nn.Linear(162, 31)
 
         # nn.init.xavier_normal_(self.tgt_word_prj.weight)
@@ -70,13 +41,20 @@ class TextLSTM(nn.Module):
         batch_as = torch.nn.functional.one_hot(batch_as, VOCAB_SZ)
         batch_as = batch_as.float()
 
-        hidden_state = Variable(
-            torch.zeros(1, batch_size, num_hidden, dtype=torch.cuda.FloatTensor)
-        )
-        cell_state = Variable(torch.zeros(1, batch_size, num_hidden, dtype=torch.float))
         batch_qs = batch_qs.float()  # (162, 16, 95)
 
+        hidden_state = Variable(
+            torch.zeros(1, batch_size, self.num_hidden, dtype=torch.float)
+        )
+        cell_state = Variable(
+            torch.zeros(1, batch_size, self.num_hidden, dtype=torch.float)
+        )
         output_seq = torch.empty((MAX_ANSWER_SZ - 1, 16, VOCAB_SZ))
+
+        if torch.cuda.is_available():
+            hidden_state = hidden_state.cuda()
+            cell_state = cell_state.cuda()
+            output_seq = output_seq.cuda()
 
         # Input question phase
         for t in range(MAX_QUESTION_SZ):
@@ -84,7 +62,7 @@ class TextLSTM(nn.Module):
                 batch_qs[t].unsqueeze(0), (hidden_state, cell_state)
             )
         # Extra 15 Computational Steps
-        dummy_input = torch.zeros(1, batch_size, VOCAB_SZ, dtype=torch.cuda.FloatTensor)
+        dummy_input = torch.zeros(1, batch_size, VOCAB_SZ, dtype=torch.float)
         for t in range(15):
             outputs_junk, (hidden_state, cell_state) = self.lstm(
                 dummy_input, (hidden_state, cell_state)
@@ -114,57 +92,93 @@ class TextLSTM(nn.Module):
         return output_seq.reshape(-1, batch_ans_size)
 
 
-model = TextLSTM()
+def main():
+    # CUDA for PyTorch
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    print(device)
+    # cudnn.benchmark = True
+
+    # Uni-LSTM(Attention) Parameters
+    num_workers = 2
+
+    exp_name = "math_test"
+    unique_id = "02-24-2020"
+
+    tb = Tensorboard(exp_name, unique_name=unique_id)
+
+    ds_train = FullDatasetManager(
+        "./mathematics_dataset-v1.0", max_elements=10, deterministic=True,
+    )
+    train_loader = torch.utils.data.DataLoader(
+        ds_train,
+        batch_size=16,
+        shuffle=True,
+        num_workers=num_workers,
+        collate_fn=lstm_batch_collate_fn,
+    )
+
+    model = TextLSTM()
+
+    # Specify optimizations algs
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    if torch.cuda.device_count() > 1:
+        print("Using", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
+        model = model.cuda()
+
+    model = model.to(device)
+
+    # model_process.train(
+    #     exp_name=exp_name,
+    #     unique_id=unique_id,
+    #     model=model,
+    #     training_data=train_loader,
+    #     optimizer=optimizer,
+    #     device=device,
+    #     epochs=1,
+    #     tb=tb,
+    #     max_batches=max_batches,
+    #     validation_data=None,
+    # )
+    # TODO:
+    # Rejigger weight loss calculation as it is different from transformer.
+    model_process.train(
+        name=f"{exp_name}_{unique_id}",
+        model=model,
+        training_data=train_loader,
+        optimizer=optimizer,
+        device=device,
+        epochs=1000,  # Not relevant, will get ended before this due to max_b
+        tb=tb,
+    )
 
 
-# Specify optimizations algs
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Ending script")
+    except BaseException:
+        print("Catching error...")
+        print(traceback.format_exc())
+        # utils.shutdown()
+        raise
 
-if torch.cuda.device_count() > 1:
-    print("Using", torch.cuda.device_count(), "GPUs!")
-    model = nn.DataParallel(model)
-    model = model.cuda()
+    # Training (Old School)
+    # for epoch in range(1000):
+    #     optimizer.zero_grad()
 
-model = model.to(device)
+    #     output = model(input_batch)
+    #     loss = criterion(output, target_batch)
+    #     if (epoch + 1) % 100 == 0:
+    #         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
 
-# model_process.train(
-#     exp_name=exp_name,
-#     unique_id=unique_id,
-#     model=model,
-#     training_data=train_loader,
-#     optimizer=optimizer,
-#     device=device,
-#     epochs=1,
-#     tb=tb,
-#     max_batches=max_batches,
-#     validation_data=None,
-# )
-# TODO:
-# Rejigger weight loss calculation as it is different from transformer.
-model_process.train(
-    name=f"{exp_name}_{unique_id}",
-    model=model,
-    training_data=train_loader,
-    optimizer=optimizer,
-    device=device,
-    epochs=1000,  # Not relevant, will get ended before this due to max_b
-    tb=tb,
-)
+    #     loss.backward()
+    #     optimizer.step()
 
-# Training (Old School)
-# for epoch in range(1000):
-#     optimizer.zero_grad()
+    # inputs = [sen[:3] for sen in seq_data]
 
-#     output = model(input_batch)
-#     loss = criterion(output, target_batch)
-#     if (epoch + 1) % 100 == 0:
-#         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
-
-#     loss.backward()
-#     optimizer.step()
-
-# inputs = [sen[:3] for sen in seq_data]
-
-# predict = model(input_batch).data.max(1, keepdim=True)[1]
-# print(inputs, '->', [number_dict[n.item()] for n in predict.squeeze()])
+    # predict = model(input_batch).data.max(1, keepdim=True)[1]
+    # print(inputs, '->', [number_dict[n.item()] for n in predict.squeeze()])
